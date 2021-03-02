6f8c1026eb350f128c13ef69520b75baf6719aed
adrien <adrien.s@laposte.net>
2021-03-02 11:13:39 +0100

    firstCommit

---
+++ salaire/script/script.js
@@ -0,0 +1,48 @@
+let salaries = [
+ {
+   firstName: 'Daniel',
+   lastName: 'Girard',
+   brut: 2100,
+   age: 19,
+   numberInCharge: 0,
+   mdph: true,
+   prime: true,
+   allocation: false
+ }, {
+   firstName: 'Jean',
+   lastName: 'Dupont',
+   brut: 3500,
+   age: 45,
+   numberInCharge: 5,
+   mdph: false,
+   prime: false,
+   allocation:  false
+ }, {
+   firstName: 'Lois',
+   lastName: 'Blair',
+   brut: 4500,
+   age: 54,
+   numberInCharge: 2,
+   mdph: false,
+   prime: false,
+   allocation: true
+ }, {
+   firstName: 'Jules',
+   lastName: 'Verne',
+   brut: 950,
+   age: 28,
+   numberInCharge: 0,
+   mdph: false,
+   prime: true,
+   allocation: true
+ }, {
+   firstName: 'Jean',
+   lastName: 'Jacques',
+   brut: 1200,
+   age: 26,
+   numberInCharge: 1,
+   mdph: false,
+   prime: false,
+   allocation: true
+ }
+];
